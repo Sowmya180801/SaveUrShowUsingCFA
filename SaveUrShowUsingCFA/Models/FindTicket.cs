d28b98f2cea3cb327f84dda9e5ff685b664fd666
Sowmya <sowmyap1881@gmail.com>
2023-03-15 15:38:48 +0530

    Folder is added

---
+++ SaveUrShowUsingCFA/Models/FindTicket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaveUrShowUsingCFA.Models
+{
+    public class FindTicket
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Location { get; set; }
+        public string Theatrename { get; set; }
+        [Required(ErrorMessage = "Movie name is reqd...!!")]
+        public string Moviename { get; set; }
+        public string MovieLink { get; set; }
+        public string Slot { get; set; }
+        public int charges { get; set; }
+        //[Required(ErrorMessage = "Date is reqd...!!")]
+        public DateTime? Date { get; set; }
+    }
+}
---
+++ SaveUrShowUsingCFA/Models/SaveUrShowUsingCFADbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaveUrShowUsingCFA.Models
+{
+    public class SaveUrShowUsingCFADbContext : DbContext
+    {
+        public SaveUrShowUsingCFADbContext(DbContextOptions<SaveUrShowUsingCFADbContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Registration> Registration { get; set; }
+        public virtual DbSet<FindTicket> FindTicket { get; set; }
+        public virtual DbSet<BookTicket> BookTicket { get; set; }
+    }
+}
